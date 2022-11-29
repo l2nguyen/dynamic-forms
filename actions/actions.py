7ff1fe834c595e4f31b9377ec3948f72e6f43eef
Lena Nguyen <lena.nguyen4@gmail.com>
2022-11-29 14:28:06 +0000

    modify required_slots based on user response

--- actions/actions.py
+++ actions/actions.py
@@ -2,9 +2,10 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
 
 
-class ValidateRecoverPassword(FormValidationAction):
+class ValidateRecoverPasswordForm(FormValidationAction):
     """Example of a form validation action."""
 
     def name(self) -> Text:
@@ -26,3 +27,32 @@ class ValidateRecoverPassword(FormValidationAction):
             additional_slots.append("known_email")
 
         return additional_slots + domain_slots
+
+class ValidateExampleFlowForm(FormValidationAction):
+    """Example of a form validation action."""
+
+    def name(self) -> Text:
+        return "validate_example_flow_form"
+
+    async def required_slots(
+        self,
+        domain_slots: List[Text],
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> List[Text]:
+        # see: https://rasa.com/docs/rasa/forms/#dynamic-form-behavior
+
+        additional_slots = []
+        
+        if tracker.slots.get("question1") is True:
+            # If the user doesn't know the answer to the safety question, ask
+            # if they know the associated email
+            additional_slots.append("question2")
+            
+        if tracker.slots.get("question2") is True:
+            # If the user doesn't know the answer to the safety question, ask
+            # if they know the associated email
+            additional_slots.append("question3")
+
+        return domain_slots + additional_slots
